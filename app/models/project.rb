dfb1888465c3acfc0484811b586b3854ec359fe8
Alex <alexandertraffis@gmail.com>
2022-11-24 02:04:26 +0000

    Iteration 04 Stuff

--- app/models/project.rb
+++ app/models/project.rb
@@ -1,4 +1,4 @@
 class Project < ApplicationRecord
     # Validates that there is a title and description
-    validates_presence_of :title, :description
+    # validates_presence_of :title, :description
 end
